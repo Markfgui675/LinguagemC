a96505cc1f469139ec6ee5d7a1b9b09c2f682a36
Marcos Fellipe Guimarães <85682890+Markfgui675@users.noreply.github.com>
2022-12-21 11:04:44 -0300

    Printf

---
+++ printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <locale.h>
+/*
+    Comentário
+*/
+int main(){
+    
+    setlocale(LC_ALL,"Portuguese");
+    printf("Hello, world!\n");
+    printf("Olá, mundo!");
+
+}
